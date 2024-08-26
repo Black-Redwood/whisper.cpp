d9e04a694fc6889f6f2f39b16661028ef9a5ed30
hotpocket <githotpocket@gmail.com>
2024-08-26 03:39:30 -0700

    fix path to binary & add path to model. remove stdout as this gets streamed to the client.

--- transcriptions/transcribe.sh
+++ transcriptions/transcribe.sh
@@ -2,10 +2,9 @@
 
 # Function to clean up temporary files
 cleanup() {
-    echo "Cleaning up temporary files..."
+    # echo "Cleaning up temporary files..."
     rm -f "$file" "$file.wav"
 }
-
 # Trap to catch signals and clean up
 trap cleanup EXIT INT TERM
 
@@ -13,7 +12,7 @@ base="`date +"%m-%d-%Y_%H%M%S"`_youtube"
 yt-dlp "$1" -o "$base" -x  > /dev/null 2>&1
 file=`/bin/ls | grep "$base"`    # we don't know what audio file extension yt-dlp will create
 ffmpeg -i  ./"$file" -acodec pcm_s16le -ac 1 -ar 16000 "$file.wav" > /dev/null 2>&1
-./main "${file}.wav" 2> /dev/null
+../main  -m ../models/ggml-base.bin "${file}.wav" 2> /dev/null
 
 # Clean up normally if the script completes successfully
 cleanup
