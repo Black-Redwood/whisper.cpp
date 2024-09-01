13eafabb1770961e9e6c05d1321b359689c93a19
hotpocket <githotpocket@gmail.com>
2024-09-01 02:20:58 -0700

    use cookies file now and report errors if they happen.

--- transcriptions/transcribe.sh
+++ transcriptions/transcribe.sh
@@ -3,16 +3,25 @@
 # Function to clean up temporary files
 cleanup() {
     # echo "Cleaning up temporary files..."
-    rm -f "$file" "$file.wav"
+    rm -f "$file" "$file.wav" tmp_cookies.txt
 }
 # Trap to catch signals and clean up
 trap cleanup EXIT INT TERM
 
 base="`date +"%m-%d-%Y_%H%M%S"`_youtube"
-yt-dlp "$1" -o "$base" -x  > /dev/null 2>&1
+# yt-dlp will modify our cookies file, so make a tmp throwaway copy
+touch cookies.txt
+cp cookies.txt tmp_cookies.txt
+# Download the video and extract the audio
+yt-dlp --cookies tmp_cookies.txt "$1" -o "$base" -x  > /dev/null 2>&1
+[ $? -ne 0 ] && exit -1
+# Find the audio file yt-dlp created
 file=`/bin/ls | grep "$base"`    # we don't know what audio file extension yt-dlp will create
+# Convert the audio to the correct format for transcribing
 ffmpeg -i  ./"$file" -acodec pcm_s16le -ac 1 -ar 16000 "$file.wav" > /dev/null 2>&1
+[ $? -ne 0 ] && exit -1
+# Transcribe the audio
 ../main  -m ../models/ggml-base.bin "${file}.wav" 2> /dev/null
-
+[ $? -ne 0 ] && exit -1
 # Clean up normally if the script completes successfully
 cleanup
